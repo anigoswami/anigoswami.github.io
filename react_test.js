2bcb89894e26770073c3f73a9b0214d583114aa0
anigoswami <agoswami@mun.ca>
2022-10-11 20:58:39 +0530

    Fixed images not loading issue

---
+++ react_test.js
@@ -0,0 +1,29 @@
+//Starter code
+
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+//lines pasted after
+const domContainer = document.querySelector('#react_test');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(LikeButton));
