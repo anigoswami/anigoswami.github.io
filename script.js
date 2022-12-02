5204e3c06ebb224161416bf9322b5d15058a2fcf
Anirban Goswami <agoswami@mun.ca>
2022-12-02 15:03:27 -0330

    incremental-improvement
    Added js for new row support, increased mobile friendliness, made the vibes a bit better. All that's left to do is add the project imgs and captions. there needs to be 6 in total and 1 already exists, so need 5 more.

---
+++ script.js
@@ -0,0 +1,5 @@
+function showMore(){
+    document.getElementById("magazine2").style.display="flex";
+    document.getElementById("more").style.display="none";
+    document.getElementById("work").style.height="auto";
+};
