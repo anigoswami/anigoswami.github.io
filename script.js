7a018c2c62aba120b5b8c697e2d966ed96dc5a3e
Anirban Goswami <agoswami@mun.ca>
2022-12-15 10:45:37 -0330

    cleaned up code

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ function showMore(){
     document.getElementById("more").style.display="none";
     document.getElementById("work").style.height="auto";
 };
+
+function takeimg(){
+    img=document.getElementsByClassName("mag-img");
+    return img;
+};
+
+function changeimg(i,n){
+    
+    return i[n];
+    
+}
+
+function taketext(){
+    txt=document.getElementById("img-caption");
+};
+
+function testimg(){
+    console.log(takeimg());
+    console.log(changeimg(takeimg(),5));
+};
